2744640b6eec4a9f0621f896636eaf5621d606b9
Ярослав Андриенко <yandrienko@inno.tech>
2024-11-26 22:15:23 +0300

    реализовал генерацию + grpc сервер

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	desc "github.com/sadstill/chat-server/pkg/chat/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"log"
+	"net"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) SendMessage(ctx context.Context, in *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Received request: %+v", in)
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %d", grpcPort)
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sadstill/chat-server
+
+go 1.23
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+)
