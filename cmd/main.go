24a6b04584d205ebad6eb3b4ea4af8cf34709488
Ярослав Андриенко <yandrienko@inno.tech>
2024-11-27 22:12:54 +0200

    поправил замечания линтера

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	desc "github.com/sadstill/chat-server/pkg/chat/v1"
+	"log"
+	"net"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"log"
-	"net"
+
+	desc "github.com/sadstill/chat-server/pkg/chat/v1"
 )
 
 const grpcPort = 50051
